91191ba2958f90652b489819c0e738a292ef78c5
shang6723 <31940209+shang6723@users.noreply.github.com>
2018-03-08 22:40:55 -0800

    Edit layout

--- js/agency.js
+++ js/agency.js
@@ -28,11 +28,12 @@
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
+    $("#mainNav").addClass("navbar-shrink");
+    // if ($("#mainNav").offset().top > 100) {
+    //   $("#mainNav").addClass("navbar-shrink");
+    // } else {
+    //   $("#mainNav").removeClass("navbar-shrink");
+    // }
   };
   // Collapse now if page is not at top
   navbarCollapse();
